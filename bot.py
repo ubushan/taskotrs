9b097871506ce2af3cb32c749cf96ac4d889b9a9
Anatoly Ubushan <ubushaev08@gmail.com>
2017-11-08 15:14:23 +0300

    inline_button refresh

--- bot.py
+++ bot.py
@@ -265,7 +265,7 @@ def getMessage():
 def webhook():
     bot.remove_webhook()
     bot.set_webhook(url=config.properties.APP_URL)
-    return "!", 200
+    return "OK!", 200
 
 server.run(host="0.0.0.0", port=os.environ.get('PORT', 5000))
 server = Flask(__name__)
