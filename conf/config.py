c44c0897c76428a7b6d912d992b7ab1a7dea0e97
Anatoly Ubushan <ubushaev08@gmail.com>
2017-11-08 15:17:59 +0300

    refresh

--- conf/config.py
+++ conf/config.py
@@ -7,8 +7,8 @@ class properties:
     OTRS_MAIL = "otrs.task@guardant.ru"
 
     # TOKENS
-    TELEGRAM_BOT_TOKEN = "309529694:AAH9G70sC7jUFELUn7YbedELq_R4RVSNLDI"
-    SENDGRID_API_TOKEN = "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"
+    TELEGRAM_BOT_TOKEN = ""
+    SENDGRID_API_TOKEN = ""
 
     # PATH TO PROJECT
     PATH = "./"
